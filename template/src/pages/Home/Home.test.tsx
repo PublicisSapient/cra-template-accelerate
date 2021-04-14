53f61777f7f1ce3bb4fc54c3d5c359180e25f6a9
Naresh Bhatia <naresh.archfirst@gmail.com>
2021-04-13 22:45:55 -0400

    test: fixed issue with Home test

--- template/src/pages/Home/Home.test.tsx
+++ template/src/pages/Home/Home.test.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from '../../test/test-utils';
 import { Home } from './Home';
 
+// Set API_URL in window environment
+(window as any)._env_ = {
+  API_URL: 'http://localhost:8080',
+};
+
 describe('<Home />', () => {
   test('renders correctly', async () => {
     const { findByTestId, findByText } = render(<Home />);
