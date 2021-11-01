7519d5b653bb37785dd819eed88a44289623fdb9
Naresh Bhatia <naresh.archfirst@gmail.com>
2021-11-01 02:46:45 -0400

    test: updated test-utils with better custom renderer and replaced MemoryRouter with BrowserRouter

--- template/.storybook/preview.tsx
+++ template/.storybook/preview.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { addDecorator } from '@storybook/react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ErrorBoundary, Loading } from '../src/components';
 import { EnvProvider } from '../src/contexts';
 import '../src/styles/main.css';
 
@@ -18,12 +18,16 @@ const { worker } = require('../src/mocks/browser');
 worker.start();
 worker.printHandlers();
 
-const StoryDecorator = (Story: any) => (
-  <EnvProvider>
-    <Router>
-      <Story />
-    </Router>
-  </EnvProvider>
-);
-
-addDecorator(StoryDecorator);
+export const decorators = [
+  (Story: any) => (
+    <Suspense fallback={<Loading />}>
+      <ErrorBoundary>
+        <EnvProvider>
+          <Router>
+            <Story />
+          </Router>
+        </EnvProvider>
+      </ErrorBoundary>
+    </Suspense>
+  ),
+];
